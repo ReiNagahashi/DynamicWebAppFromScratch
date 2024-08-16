740c105f3a78778b1410ad12c142b6f2e30b139a
Hashi <rerenaga9170@icloud.com>
2024-08-16 18:07:06 +0900

    Initialize db connection with wrapper class

--- init-app.php
+++ init-app.php
@@ -2,12 +2,13 @@
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
+use Database\MySQLWrapper;
 use Helpers\Settings;
 
 // 接続の失敗時にエラーを例外とともに出力→これはdb接続の初期化の際は必ず実装してテストをすること
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = new mysqli('localhost', Settings::env('DATABASE_USER'),
+$mysqli = new MySQLWrapper('localhost', Settings::env('DATABASE_USER'),
 Settings::env('DATABASE_USER_PASSWORD'), Settings::env('DATABASE_NAME'));
 
 $charset = $mysqli->get_charset();
