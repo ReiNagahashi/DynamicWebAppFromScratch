7ab79d3779f9adfe14310bbd4b6d0fa5a852a141
Hashi <rerenaga9170@icloud.com>
2024-08-22 16:18:04 +0200

    Refactor of data insertion with sprintf

--- Database/insert.php
+++ Database/insert.php
@@ -3,39 +3,59 @@ namespace Database;
 
 use Database\MySQLWrapper;
 use Exception;
+use mysqli;
 
 $mysqli = new MySQLWrapper();
+// 各カラム値を引数とした関数を作ってsprintfを返すことでクリーンな実装になるよ！
+function insertCarQuery(
+    string $make,
+    string $model,
+    int $year,
+    string $color,
+    float $price,
+    float $mileage,
+    string $transmission,
+    string $engine,
+    string $status
+): string{
+    return sprintf(
+        "INSERT INTO cars(make, model, year, color, price, mileage, transmission, engine, status)
+        VALUES('%s', '%s', '%d', '%s', '%f', '%f', '%s', '%s', '%s');",
+        $make, $model, $year, $color, $price, $mileage, $transmission, $engine, $status
+    );
+}
 
-// 直感的な方法
-$car_insert_query = "
-    INSERT INTO cars (make, model, year, color, price, mileage, transmission, engine, status)
-    VALUES ('Toyota', 'Corolla', 2020, 'Blue', 20000, 1500, 'Automatic', 'Gasoline', 'Available'),
-        ('Honda', 'Civic', 2019, 'Red', 18500, 1200, 'Manual', 'Gasoline', 'Sold');
-";
 
-$result = $mysqli->query($car_insert_query);
-if($result === false) throw new Exception('Could not execute car insertion query.');
+function insertPartQuery(
+    string $name,
+    string $description,
+    float $price,
+    int $quantityInStock
+): string {
+    return sprintf(
+        "INSERT INTO parts (name, description, price, quantityInStock)
+        VALUES ('%s', '%s', %f, %d);",
+        $name, $description, $price, $quantityInStock
+    );
+}
 
 
-// 今は手動で中間テーブルに格納している
-$part_insert_query = "
-    INSERT INTO parts (name, description, price, quantityInStock)
-    VALUES ('Brake Pad', 'High Quality Brake Pad', 45.99, 100),
-        ('Oil Filter', 'Long-lasting Oil Filter', 10.99, 200);
-";
+function runQuery(mysqli $mysqli, string $query): void{
+    $result = $mysqli->query($query);
+    if($result === false){
+        throw new Exception('Could not excute query.');
+    }else{
+        echo "Query exuted successfully. \n";
+    }
+}
 
-$result = $mysqli->query($part_insert_query);
-if($result === false) throw new Exception('Could not execute part insertion query.');
+runQuery($mysqli, insertCarQuery('Toyota', 'Corolla', 2020, 'Blue', 200000, 1500, 'Automatic', 'Gasoline', 'Available'));
 
-$car_part_insert_query = "
-    INSERT INTO cars_parts (carID, partID, quantity)
-    VALUES (1, 1, 4),
-        (1, 2, 1);
-";
-
-$result = $mysqli->query($car_part_insert_query);
-if($result === false) throw new Exception('Could not execute car-part insertion query.');
-
-echo "Data insertion successful.";
+runQuery($mysqli, insertPartQuery(
+    name: 'Brake Pad',
+    description: 'High Quality Brake Pad',
+    price: 45.99,
+    quantityInStock: 100
+));
 
 $mysqli->close();
