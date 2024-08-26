f64586b25ccf09278d8232e7d16c171dc8b09c5d
Hashi <rerenaga9170@icloud.com>
2024-08-26 16:16:17 +0200

    Create tables for basic SNS app
    Tables: users, posts, comments, likes, tags, categories

--- Database/insert.php
+++ Database/insert.php
@@ -40,22 +40,64 @@ function insertPartQuery(
 }
 
 
+function insertUserQuery(
+    string $username,
+    string $email,
+    string $password,
+    string $created_at,
+    string $updated_at,
+    string $subscription,
+    string $subscription_status,
+    string $subscription_created_at,
+    string $subscription_end_at
+){
+    return sprintf(
+        "INSERT INTO users (user_name, email, password, created_at, updated_at, subscription,
+        subscription_status, subscription_created_at, subscription_end_at)
+        VALUES ('%s', '%s','%s', '%s','%s',
+            '%s','%s', '%s','%s');",
+            $username, $email, $password, $created_at, $updated_at, $subscription,
+            $subscription_status, $subscription_created_at, $subscription_end_at 
+    );
+}
+
+
+function insertPostQuery(
+    string $title,
+    string $content,
+    string $created_at,
+    string $updated_at,
+    int $user_id
+){
+    return sprintf(
+        "INSERT INTO posts(title, content, created_at, updated_at, user_id)
+        VALUES ('%s', '%s', '%s', '%s', '%d');",
+        $title, $content, $created_at, $updated_at, $user_id
+    );
+}
+
+
 function runQuery(mysqli $mysqli, string $query): void{
     $result = $mysqli->query($query);
     if($result === false){
         throw new Exception('Could not excute query.');
     }else{
-        echo "Query exuted successfully. \n";
+        echo "Query excuted successfully. \n";
     }
 }
 
-runQuery($mysqli, insertCarQuery('Toyota', 'Corolla', 2020, 'Blue', 200000, 1500, 'Automatic', 'Gasoline', 'Available'));
+// runQuery($mysqli, insertCarQuery('Toyota', 'Corolla', 2020, 'Blue', 200000, 1500, 'Automatic', 'Gasoline', 'Available'));
 
-runQuery($mysqli, insertPartQuery(
-    name: 'Brake Pad',
-    description: 'High Quality Brake Pad',
-    price: 45.99,
-    quantityInStock: 100
-));
+// runQuery($mysqli, insertPartQuery(
+//     name: 'Brake Pad',
+//     description: 'High Quality Brake Pad',
+//     price: 45.99,
+//     quantityInStock: 100
+// ));
+
+
+// runQuery($mysqli, insertUserQuery('Rei', 'akiba@dmail.com', 'abcdefg', '3033-12-21', '4020-12-24', "yes", "silver", "3033-12-21", "3033-12-30"));
+
+runQuery($mysqli, insertPostQuery('Tate no kai', 'Finally Japan is collapsed!', "2025-01-23", "2030-01-01", 1));
 
 $mysqli->close();
