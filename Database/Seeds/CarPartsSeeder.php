8b6461717feb6bf00afd4285ff32b18cb3672f26
Hashi <rerenaga9170@icloud.com>
2024-10-23 16:34:03 +0200

    Debug for the function which gets ids from table

--- Database/Seeds/CarPartsSeeder.php
+++ Database/Seeds/CarPartsSeeder.php
@@ -5,6 +5,7 @@ use Database\AbstractSeeder;
 use Database\MySQLWrapper;
 use Exception;
 use Faker\Factory;
+use Helpers\HelperFunction;
 
 class CarPartsSeeder extends AbstractSeeder{
     // TODO: tableName文字列を割り当ててください
@@ -35,36 +36,11 @@ class CarPartsSeeder extends AbstractSeeder{
 
     public function createRowData(): array{
         $listOfCarParts = $this->generateListOfCarPartsData();
-
+        print_r($listOfCarParts);
         return $listOfCarParts;
     }
 
-    private static function getCarIds(): array{
-        $mysqli = new MySQLWrapper();
 
-        // クエリをprepareステートメントで行うことでSQLインジェクション対策になるようだ 
-        $stmt = $mysqli->prepare("SELECT id FROM cars");
-        if($stmt === false){
-            throw new Exception('Could not excute query.');
-        }
-
-        // クエリの実行
-        $stmt->execute();
-        // 結果の保存
-        $stmt->store_result();
-        // 取得データが存在するか
-        // ⚠️bind_resultを使う必要がある
-        $stmt->bind_result($id);
-        $carIds = [];
-        while($stmt->fetch()){
-            $carIds[] = ['id' => $id];
-        }
-
-        $stmt->close();
-        $mysqli->close();
-
-        return $carIds;
-    }
 
     private static function generateCarPartsData(int $carId): array{
         $faker = Factory::create();
@@ -83,7 +59,7 @@ class CarPartsSeeder extends AbstractSeeder{
         $carParts = [];
         // ここにcarsテーブルからidを全て取得した配列を用意する。
         // その上で、その配列内の要素をランダムに取得した値を外部キーとする
-        $forgein_keys = self::getCarIds();
+        $forgein_keys = HelperFunction::getIdsFromTable("cars");
 
         if(count($forgein_keys) == 0) return [];
 
